086c8fa10ba904b4e438f6c3e411c20a45b5b590
Azam Azam <mdazam@microsoft.com>
2020-08-31 13:33:26 -0700

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  greetings = "Hello There";
+  placeHolderText = "Enter your name";
+  users = [
+    {userId: 10, name: 'Jon'},
+    {userId: 11, name: 'Steve'},
+    {userId: 12, name: 'Bob'},
+  ];
+  todos = [
+    {id: 1, content: "Buy milk"},
+    {id: 2, content: "Buy Bread"},
+  ];
+  
   title = 'demo-angular';
+  showMessage = true;
+  myColor = 'red';
+  readMe = () => {
+    console.log('button clicked');
+    this.title = "Changed Title to NOTHING";
+  }
 }
